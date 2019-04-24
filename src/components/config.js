d6b171277b616311a64d260038ebdfb49f4a3938
Joseph <jtu4@lion.lmu.edu>
2019-04-24 14:13:13 -0700

    first commit

---
+++ src/components/config.js
@@ -0,0 +1,5 @@
+const tokenUrl =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/e08c5141-7780-429d-8ffb-5858217d8e5d/token";
+const instanceLocator = "v1:us1:e08c5141-7780-429d-8ffb-5858217d8e5d";
+
+export { tokenUrl, instanceLocator };
